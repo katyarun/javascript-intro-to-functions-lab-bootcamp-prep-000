30458ed7195c5d5276fb62ee84225119ddbd7d81
katyarun@gmail.com <katyarun@gmail.com>
2018-04-18 01:19:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-  
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+}
+
+function sayHiToGrandma(string){
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
 }
